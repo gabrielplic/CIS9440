9bffe5ba28869d20a2532bd2c11ce120c7929f59
gabrielplic <gabriel.plic@baruchmail.cuny.edu>
2024-05-12 15:41:23 -0400

    altered physical schema

--- scripts/Real_Estate_Sales.sql
+++ scripts/Real_Estate_Sales.sql
@@ -1,70 +1,62 @@
-CREATE SCHEMA INSTANCE;
+-- Create the schema
+CREATE SCHEMA RE_Sales;
 
-CREATE TABLE Real_Estate_Sales_dw_lgl.INSTANCE.dim_date ( 
-	date_id int64 NOT NULL  ,
-	yearNumber int64  ,
-	monthNumber int64  ,
-	quarter int64  ,
-	dayNumber int64  ,
-	dayName int64  ,
-	monthName int64  ,
-	weekoftheMonth int64  ,
-	weekoftheYear int64  ,
-	dateIsoformat datetime  
- );
+-- Create dim_date table
+CREATE TABLE RE_Sales.dim_date (
+	date_id INT NOT NULL,
+	date_iso_format DATE,
+	year_number INT,
+	month_number INT,
+	day_number INT,
+	quarter_number INT,
+	month_name VARCHAR(100),
+	day_name VARCHAR(100),
+	week_of_year INT,
+	week_of_month INT
+);
 
-ALTER TABLE Real_Estate_Sales_dw_lgl.INSTANCE.dim_date ADD PRIMARY KEY ( date_id )  NOT ENFORCED;
+-- Add primary key to dim_date
+ALTER TABLE RE_Sales.dim_date ADD PRIMARY KEY (date_id);
 
-CREATE TABLE Real_Estate_Sales_dw_lgl.INSTANCE.dim_location ( 
-	location_id int64 NOT NULL  ,
-	town int64  ,
-	address int64  ,
-	latitude bignumeric  ,
-	longitude bignumeric  
- );
+-- Create dim_location table
+CREATE TABLE RE_Sales.dim_location (
+	location_id INT NOT NULL,
+	town VARCHAR(255), -- Adjusted if town names or other string data are stored
+	address VARCHAR(255) -- Adjusted if full addresses are to be stored
+);
 
-ALTER TABLE Real_Estate_Sales_dw_lgl.INSTANCE.dim_location ADD PRIMARY KEY ( location_id )  NOT ENFORCED;
+-- Add primary key to dim_location
+ALTER TABLE RE_Sales.dim_location ADD PRIMARY KEY (location_id);
 
-CREATE TABLE Real_Estate_Sales_dw_lgl.INSTANCE.dim_nonUseCode ( 
-	nonUseCode_id int64 NOT NULL  ,
-	nonUseCode int64  
- );
+-- Create dim_propertyType table
+CREATE TABLE RE_Sales.dim_propertyType (
+	propertyType_id INT NOT NULL,
+	propertyType VARCHAR(255) -- Adjusted if it's a string code or description
+);
 
-ALTER TABLE Real_Estate_Sales_dw_lgl.INSTANCE.dim_nonUseCode ADD PRIMARY KEY ( nonUseCode_id )  NOT ENFORCED;
+-- Add primary key to dim_propertyType
+ALTER TABLE RE_Sales.dim_propertyType ADD PRIMARY KEY (propertyType_id);
 
-CREATE TABLE Real_Estate_Sales_dw_lgl.INSTANCE.dim_propertyType ( 
-	propertyType_id int64 NOT NULL  ,
-	propertyType int64  
- );
+-- Create dim_residentialType table
+CREATE TABLE RE_Sales.dim_residentialType (
+	residentialType_id INT NOT NULL,
+	residentialType VARCHAR(255) -- Adjusted if it's a string code or description
+);
 
-ALTER TABLE Real_Estate_Sales_dw_lgl.INSTANCE.dim_propertyType ADD PRIMARY KEY ( propertyType_id )  NOT ENFORCED;
+-- Add primary key to dim_residentialType
+ALTER TABLE RE_Sales.dim_residentialType ADD PRIMARY KEY (residentialType_id);
 
-CREATE TABLE Real_Estate_Sales_dw_lgl.INSTANCE.dim_remarks ( 
-	remarks_id int64 NOT NULL  ,
-	assessorRemarks int64  ,
-	opmRemarks int64  
- );
+-- Create facts_RESales table
+CREATE TABLE RE_Sales.facts_RESales (
+	fact_id INT NOT NULL,
+	sale_amount numeric,
+	assessed_value numeric,
+	sales_ratio numeric,
+	location_id INT NOT NULL,
+	propertyType_id INT,
+	residentialType_id INT,
+	date_id INT
+);
 
-ALTER TABLE Real_Estate_Sales_dw_lgl.INSTANCE.dim_remarks ADD PRIMARY KEY ( remarks_id )  NOT ENFORCED;
-
-CREATE TABLE Real_Estate_Sales_dw_lgl.INSTANCE.dim_residentialType ( 
-	residentialType_id int64 NOT NULL  ,
-	residentialType int64  
- );
-
-ALTER TABLE Real_Estate_Sales_dw_lgl.INSTANCE.dim_residentialType ADD PRIMARY KEY ( residentialType_id )  NOT ENFORCED;
-
-CREATE TABLE Real_Estate_Sales_dw_lgl.INSTANCE.facts_RESales ( 
-	fact_id int64 NOT NULL  ,
-	sale_amount numeric  ,
-	assessed_value numeric  ,
-	sales_ratio numeric  ,
-	location_id int64 NOT NULL  ,
-	propertyType_id int64  ,
-	residentialType_id int64  ,
-	nonUseCode_id int64  ,
-	remarks_id int64  ,
-	date_id int64  
- );
-
-ALTER TABLE Real_Estate_Sales_dw_lgl.INSTANCE.facts_RESales ADD PRIMARY KEY ( fact_id, location_id )  NOT ENFORCED;
+-- Add primary key to facts_RESales
+ALTER TABLE RE_Sales.facts_RESales ADD PRIMARY KEY (fact_id, location_id);
